d14653c8ce1e01210707b02d8caef93d7c03a774
Abhishek Choudhary <abhi@Abhisheks-MacBook-Pro.local>
2021-12-28 19:52:47 +0530

    React Intro

---
+++ src/Button/ButtonDisplay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from "./Button" 
+
+const ButtonDisplay=()=>{
+  return(
+      <div> 
+        <Button buttonText="Apple" /> 
+        <Button buttonText="Banana"/>
+        <Button buttonText="Gauava"/>
+      </div>
+  )
+}
+export default ButtonDisplay
